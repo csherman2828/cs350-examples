65b26d920f871cd6960c17eb8fcd0b3bff7d1f65
csherman2828 <csherman2828@gmail.com>
2021-10-08 16:32:02 -0500

    Improve multifile with extern example

--- 07-MultiFileWithExtern/store_number.s
+++ 07-MultiFileWithExtern/store_number.s
@@ -1,6 +1,6 @@
       .data
 prmt: .asciiz "What is your number: "
-num1: .word 0
+num1: .word 0xFFFFFFFF
       .extern num 4
 
       .text
@@ -16,10 +16,12 @@ main:
       syscall
 
                         # store the number in memory
-      sw $v0, num1
+      la $t0, num1      # load address of .data memory labelled by "num1"
+      sw $v0, 0($t0)    # store the content of $v0 into
 
                         # store the number in global space
-      sw $v0, num
+      la $t1, num
+      sw $v0, 0($t1)
 
                         # go to the hello file
       j print
